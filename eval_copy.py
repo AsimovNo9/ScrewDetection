d9dc18e69a654c348ad2e38a851ee34ecd2c0903
Adedamola Sode <adedamola.sode@gmail.com>
2022-06-28 10:31:24 +0200

    Some documentation

--- eval_copy.py
+++ eval_copy.py
@@ -10,16 +10,4 @@ model = Prepare(
     "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz"
 )
 
-# model.annotate()  # Creates label and record files
-
-# model.download_model()  # Downloads model corresponding to link passed as argument
-
-# model.update_config()  # Updates the pipeline confifuration file in the model folder of the desired model
-
-# model.train_model()  # Train the model using th pipeline configuration file to derive new checkpoints
-
 model.evaluate()  # evaluate the metrics of the model
-
-# detection_model = model.load() #Load the model and save in a  variable detection_model
-
-# model.realtime_detect(detection_model) # Start real time detection using the loaded model
--- scripts/set.py
+++ scripts/set.py
@@ -1,8 +1,8 @@
 import os
 
+import numpy as np
 import cv2
 import tensorflow as tf
-import numpy as np
 import pyautogui as pya
 from tensorflow.python.keras.utils.data_utils import get_file
 from object_detection.utils import config_util, label_map_util
@@ -14,6 +14,11 @@ from google.protobuf import text_format
 
 class Prepare:
     def __init__(self, modelURL):
+        """Prepare constructs the model variables, particularly workspace paths
+
+        Args:
+            modelURL (string): Tensorflow model zoo url, or compatible model type from a website
+        """
 
         self.modelURL = modelURL
 
@@ -35,6 +40,8 @@ class Prepare:
         self.eval_path = self.checkpoint_path + "/eval"
 
     def annotate(self):
+        """Creates the required annotations, such as the label.pbtxt file, and tf record files"""
+
         labels = [{"name": "screw", "id": 1}]
 
         with open(self.annotation_path + "\label_map.pbtxt", "w") as f:
@@ -44,15 +51,15 @@ class Prepare:
                 f.write("\tid:{}\n".format(label["id"]))
                 f.write("}\n")
 
-        # command_train = f"python {self.scripts_path}/generate_tfrecord.py -i {self.image_path}/train -l {self.annotation_path}/label_map.pbtxt -c {self.annotation_path}/Images_aug.csv -o {self.annotation_path}/train.record"
-        # command_train = f"python {self.scripts_path}/csv_to_tfrecords.py -p {self.annotation_path}/Images_aug.csv --out_dir {self.annotation_path}"
-        # os.system(command_train)
-        command_test = f"python {self.scripts_path}/generate_tfrecord.py -x {self.image_path}/test -l {self.annotation_path}/label_map.pbtxt -o {self.annotation_path}/test.record"
-        os.system(command_test)
+        command_valid = f"python {self.scripts_path}/generate_tfrecord.py -x {self.image_path}/valid -l {self.annotation_path}/label_map.pbtxt -o {self.annotation_path}/valid.record"
+        os.system(command_valid)
         command_train = f"python {self.scripts_path}/generate_tfrecord.py -x {self.image_path}/train -l {self.annotation_path}/label_map.pbtxt -o {self.annotation_path}/train.record"
         os.system(command_train)
+        command_test = f"python {self.scripts_path}/generate_tfrecord.py -x {self.image_path}/test -l {self.annotation_path}/label_map.pbtxt -o {self.annotation_path}/test.record"
+        os.system(command_test)
 
     def download_model(self):
+        """Downloads the model using the given link when creating the instance of the model prepare object"""
 
         os.makedirs(self.pretrained_model_path, exist_ok=True)
 
@@ -66,6 +73,7 @@ class Prepare:
         print("file downloaded successfully")
 
     def update_config(self):
+        """Update the pipeline config file for user needs, such as batch number, training steps, number of classes and description of annotation file destinations"""
         config = config_util.get_configs_from_pipeline_file(self.config_path)
         pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
         with tf.io.gfile.GFile(self.config_path, "r") as f:
@@ -91,52 +99,35 @@ class Prepare:
             self.annotation_path + "/label_map.pbtxt"
         )
         pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [
-            self.annotation_path + "/train.record"
+            self.annotation_path + "/valid.record"
         ]
-        # pipeline_config.train_config.optimizer.momentum_optimizer.learning_rate.cosine_decay_learning_rate.learning_rate_base = (
-        #     0.0005
-        # )
-        # pipeline_config.train_config.optimizer.momentum_optimizer.learning_rate.cosine_decay_learning_rate.warmup_learning_rate = (
-        #     0.0001
-        # )
-
         config_text = text_format.MessageToString(pipeline_config)
         with tf.io.gfile.GFile(self.config_path, "wb") as f:
             f.write(config_text)
 
     def train_model(self):
+        """Set GPU memory limit and start training model"""
 
         gpus = tf.config.experimental.list_physical_devices("GPU")
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
-        # gpus = tf.config.experimental.list_physical_devices("GPU")
-        # if gpus:
-        #     try:
-        #         tf.config.experimental.set_virtual_device_configuration(
-        #             gpus[0],
-        #             [
-        #                 tf.config.experimental.VirtualDeviceConfiguration(
-        #                     memory_limit=2048
-        #                 )
-        #             ],
-        #         )
-        #     except RuntimeError as e:
-        #         print(e)
-
-        command = f"python {self.apimodel_path}/research/object_detection/model_main_tf2.py --model_dir={self.model_path}/{self.modelName} --pipeline_config_path={self.config_path} --alsologtostderr --num_train_steps=30000"
+        command = f"python {self.apimodel_path}/research/object_detection/model_main_tf2.py --model_dir={self.model_path}/{self.modelName} --pipeline_config_path={self.config_path} --alsologtostderr --num_train_steps=25000"
         os.system(command)
 
     def evaluate(self):
-        # eval_commmand = f"tensorboard --logdir={self.model_path}"
-        # os.system(eval_commmand)
-
+        """Evaluate the model"""
         command = f"python {self.apimodel_path}/research/object_detection/model_main_tf2.py --model_dir={self.model_path}/{self.modelName} --pipeline_config_path={self.config_path} --alsologtostderr --checkpoint_dir={self.checkpoint_path}"
         os.system(command)
         eval_command = f"tensorboard --logdir={self.eval_path}"
         os.system(eval_command)
 
     def load(self):
+        """Load the most recent checkpoint as a model
+
+        Returns:
+            detection_model (ckpt): Model that can be used for inference
+        """
         configs = config_util.get_configs_from_pipeline_file(self.config_path)
         detection_model = model_builder.build(
             model_config=configs["model"], is_training=False
@@ -144,28 +135,25 @@ class Prepare:
 
         ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
         ckpt.restore(
-            os.path.join(self.checkpoint_path, "ckpt-32")
+            os.path.join(self.checkpoint_path, "ckpt-26")
         ).expect_partial()  # Restore based off last created checkpoint
         return detection_model
 
     def realtime_detect(self, _detection_model):
+        """_summary_
+
+        Args:
+            _detection_model (ckpt): Create real time detection platform using user screen
+        """
         category_index = label_map_util.create_category_index_from_labelmap(
             self.annotation_path + "/label_map.pbtxt"
         )
         detection_model = _detection_model
 
-        # cap = cv2.VideoCapture(0)
-        # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
         while True:
-            # ret, frame = cap.read()
-            # image_np = np.array(frame)
 
             image = pya.screenshot()
-            # image_np = np.array(image, dtype=np.uint8).reshape(
-            #     (image.size[1], image.size[0], 3)
-            # )
+
             image_np = np.array(image)
 
             input_tensor = tf.convert_to_tensor(
@@ -202,7 +190,6 @@ class Prepare:
             cv2.imshow("screw detection", image_np_with_detections)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
-                # cap.release()
                 cv2.destroyAllWindows()
                 break
 
